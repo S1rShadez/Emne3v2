4466a73aa6b3adbb3f3dbe6a8d52c7bb3a204d6e
S1rShadez <arthure97@hotmail.com>
2023-11-13 16:06:11 +0100

    created the first document

---
+++ Uke1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei! Hva heter du?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Velkommen {name}!");
+            Console.ReadKey();
+        }
+    }
+}
