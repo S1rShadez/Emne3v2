2f74685a2543652b401721f6c163b112bd6ec423
S1rShadez <arthure97@hotmail.com>
2023-12-20 13:42:56 +0100

    Code now gets all necessary data from the API and prints it out in a usable fashion -> next step is to save it as pokemon objects in a List and save them to a JSON document for future use

--- Uke5/PokemonAPI/Program.cs
+++ Uke5/PokemonAPI/Program.cs
@@ -45,6 +45,8 @@ namespace PokemonAPI
             //Task.WaitAll(pokemon, gen1);
             //Console.WriteLine(pokemon.Result.Name);
 
+            //List<SavePokemon> gen1List = new List<SavePokemon>();
+
 
             foreach (var item in gen1.Result.PokemonSpecies)
             {
@@ -64,37 +66,53 @@ namespace PokemonAPI
 
 
                 Console.WriteLine("\nMoves:\n");
-                foreach (var item1 in pokemon.Result.Moves)
+                foreach (var pMove in pokemon.Result.Moves)
                 {
 
                     //item1.VersionGroupDetails.ForEach(x => Console.WriteLine(x.VersionGroup.Name));
 
-                    foreach (var item2 in item1.VersionGroupDetails)
+                    foreach (var genMove in pMove.VersionGroupDetails)
                     {
-                        if (item2.VersionGroup.Name.Equals("ruby-sapphire"))
+                        if (genMove.VersionGroup.Name.Equals("ruby-sapphire"))
                         {
-                            Console.WriteLine(item1.Move.Name);
-                            Console.WriteLine(item2.VersionGroup.Name);
-                            Console.WriteLine($"Learned at lvl: {item2.LevelLearnedAt}");
-                            Console.WriteLine("Learned by: " + item2.MoveLearnMethod.Name);
+                            Console.WriteLine(pMove.Move.Name);
+                            //Console.WriteLine(item2.VersionGroup.Name);
+                            Console.WriteLine($"Learned at lvl: {genMove.LevelLearnedAt}");
+                            Console.WriteLine("Learned by: " + genMove.MoveLearnMethod.Name);
 
                             Console.WriteLine("Move stats:");
-                            var move = GetMovesAsync(item1.Move.Name);
+                            var move = GetMovesAsync(pMove.Move.Name);
 
                             Console.WriteLine("-Damage type: " + move.Result.DamageClass.Name);
-                            if (move.Result.Accuracy.HasValue) Console.WriteLine($"- Accuracy: {move.Result.Accuracy.Value}");
-                            if (move.Result.Power.HasValue) Console.WriteLine("- Power: " + move.Result.Power.Value);
+                            Console.WriteLine("-Elemental type: " + move.Result.Type.Name);
+                            if (move.Result.Accuracy.HasValue) Console.WriteLine($"-Accuracy: {move.Result.Accuracy.Value}");
+                            else Console.WriteLine("-Accuracy: 0");
+                            if (move.Result.Power.HasValue) Console.WriteLine("-Power: " + move.Result.Power.Value);
+                            else Console.WriteLine("-Power: 0");
                             Console.WriteLine("-Crit rate: " + move.Result.Meta.CritRate);
-                            if (move.Result.Pp.HasValue) Console.WriteLine("- PP: " + move.Result.Pp.Value);
+                            if (move.Result.Pp.HasValue) Console.WriteLine("-PP: " + move.Result.Pp.Value);
+                            else Console.WriteLine("-PP: 0");
                             Console.WriteLine("-Target type: " + move.Result.Target.Name);
                             Console.WriteLine("-Ailment chance: " + move.Result.Meta.AilmentChance + "%");
                             Console.WriteLine("-Stat change chance: " + move.Result.Meta.StatChance + "%");
-                            Console.WriteLine("-Flinch chance: " + move.Result.Meta.FlinchChance);
+                            Console.WriteLine("-Flinch chance: " + move.Result.Meta.FlinchChance + "%");
                             if (move.Result.EffectChance.HasValue) Console.WriteLine("-Effect chance: " + move.Result.EffectChance + "%");
+                            else Console.WriteLine("-Effect chance: 0%");
                             if (move.Result.Meta.MinTurns.HasValue) Console.WriteLine("-Min turns: " + move.Result.Meta.MinTurns.Value);
-                            if (!move.Result.Meta.MinTurns.HasValue) Console.WriteLine("-Effect lasts 1 turn");
+                            else Console.WriteLine("-Effect lasts 1 turn");
+                            if (move.Result.Meta.MaxTurns.HasValue) Console.WriteLine("-Max turns: " + move.Result.Meta.MaxTurns.Value);
+                            else Console.WriteLine("-Effect lasts 1 turn");
+                            if (move.Result.Meta.MinHits.HasValue) Console.WriteLine("-Min hits: " + move.Result.Meta.MinHits.Value);
+                            else Console.WriteLine("-Hits 1 time");
+                            if (move.Result.Meta.MaxHits.HasValue) Console.WriteLine("-Max hits: " + move.Result.Meta.MaxHits.Value);
+                            else Console.WriteLine("-Hits 1 time");
                             Console.WriteLine("-Heal amount: " + move.Result.Meta.Healing);
+                            if (move.Result.Meta.Drain > 0) Console.WriteLine("-HP drain: " + move.Result.Meta.Drain + "%");
+                            else Console.WriteLine("-HP drain: 0%");
+                            if (move.Result.Meta.Drain < 0) Console.WriteLine("-Recoil dmg: " + move.Result.Meta.Drain + "%");
+                            else Console.WriteLine("-Recoil dmg: 0%");
                             move.Result.EffectEntries.ForEach(x => Console.WriteLine("-Effect: " + x.Effect));
+
                             Console.WriteLine();
                         }
                     }
@@ -102,8 +120,72 @@ namespace PokemonAPI
 
 
 
-                Console.ReadKey();
+                //Console.ReadKey();
             }
+        }
+
+        public class SavePokemon
+        {
+            string Name;
+            List<string> BaseStats;
+            List<string> Type;
+            List<Moves> MovesList;
+
+            public SavePokemon(string name, List<string> baseStats, List<string> types, List<Moves> movesList)
+            {
+                Name = name;
+                BaseStats = baseStats;
+                Type = types;
+                MovesList = movesList;
+            }
+
+
+            public class Moves
+            {
+                string MoveName;
+                string DamageClass;
+                string ElementalType;
+                int Accuracy;
+                int Power;
+                int CritRate;
+                int Pp;
+                string TargetType;
+                int AilmentCh;
+                int StatCh;
+                int FlinchCh;
+                int EffectCh;
+                int MinTurns;
+                int MaxTurns;
+                int MinHits;
+                int MaxHits;
+                int Healing;
+                int Drain;
+                List<string> Effects;
+
+                public Moves(string moveName, string damageClass, string elementalType, int accuracy, int power, int critRate, int pp, string targetType, int ailmentCh, int statCh, int flinchCh, int effectCh, int minTurns, int maxTurns, int minHits, int maxHits, int healing, int drain, List<string> effects)
+                {
+                    MoveName = moveName;
+                    DamageClass = damageClass;
+                    ElementalType = elementalType;
+                    Accuracy = accuracy;
+                    Power = power;
+                    CritRate = critRate;
+                    Pp = pp;
+                    TargetType = targetType;
+                    AilmentCh = ailmentCh;
+                    StatCh = statCh;
+                    FlinchCh = flinchCh;
+                    EffectCh = effectCh;
+                    MinTurns = minTurns;
+                    MaxTurns = maxTurns;
+                    MinHits = minHits;
+                    MaxHits = maxHits;
+                    Healing = healing;
+                    Drain = drain;
+                    Effects = effects;
+                }
+            }
+
         }
     }
 }
